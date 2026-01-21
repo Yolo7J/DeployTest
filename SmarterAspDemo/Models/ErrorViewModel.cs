0c4f40bbb1ed9036cd1f81fb88bedcc18ccb93a5
Yolo7J <potato7morkov@gmail.com>
2026-01-21 11:14:35 +0200

    Are ve

---
+++ SmarterAspDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SmarterAspDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
